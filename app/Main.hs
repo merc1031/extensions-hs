e4cfc08cb1b7c78b87906796927f581d0f39ed01
Leonidas Loucas <lloucas@imvu.com>
2016-09-17 12:27:47 -0700

    executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,43 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
+{-# LANGUAGE PatternGuards  #-}
 module Main where
+
+import Control.Applicative ((<|>))
+import Control.Monad (void)
+import GHC.Generics
+import Options.Generic
+import qualified Data.ByteString.Char8 as BSC
 
 import Extensions
 
+data AllArgs = AllArgs {
+    file :: Maybe FilePath
+  , folders :: [FilePath]
+  , text :: Maybe BSC.ByteString
+  , extensions :: [InHaskellPragma]
+  } deriving (Generic)
+
+newtype InHaskellPragma = InHaskellPragma { unInHaskellPragma :: HaskellPragma }
+  deriving (Read)
+
+instance ParseRecord AllArgs
+
+instance ParseField InHaskellPragma
+
 main :: IO ()
-main = do
-  changeExtensions defaultExtensions "testfile.hs"
-  return ()
+main = getRecord "Pop" >>= \args -> do
+  let exts = case (map unInHaskellPragma $ extensions args) of
+          [] -> defaultExtensions
+          xs -> xs
+  go args exts
+  where go args exts
+          | Just f <- file args = changeExtensions exts f
+          | fs <- folders args
+          , True <- [] /= fs = changeMany exts fs
+          | Just content <- text args = do
+                let res = changeContent exts content
+                print res
+                return ()
+          | otherwise = return ()
--- src/Extensions.hs
+++ src/Extensions.hs
@@ -1,22 +1,64 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Extensions where
-import System.FilePath.Find (find, filePath, (==?), extension, always)
-import System.FilePath.Manip (modifyInPlace)
 
 import Control.Monad (forM, liftM, void)
+import Data.List (partition)
 import Data.Maybe (fromJust, catMaybes)
 import Data.Monoid ((<>), mconcat)
-import Data.List (partition)
-import qualified Data.Set as S
-
-
-import qualified Data.ByteString.Char8 as BSC
+import System.FilePath.Find (find, filePath, (==?), extension, always)
+import System.FilePath.Manip (modifyInPlace)
 import Text.Megaparsec
---import Text.Megaparsec.ByteString
+
+import Str (str)
+
+import qualified Data.Set as S
+import qualified Data.ByteString.Char8 as BSC
 
 type Parser = Parsec Dec BSC.ByteString
 
-defaultExtensions :: [Pragma]
+data HaskellPragma = LanguagePragma BSC.ByteString
+                   | LanguagePragmaList [BSC.ByteString]
+                   | OtherPragma BSC.ByteString
+                   deriving (Eq, Ord, Show, Read)
+
+
+exampleString :: BSC.ByteString
+exampleString = [str|{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor, 
+       DeriveAnyClass, MultiWayIf 
+       #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+module X where
+  more
+  code
+  here|]
+
+exampleLanguagePragma :: BSC.ByteString
+exampleLanguagePragma = [str|{-# LANGUAGE MultiParamTypeClasses #-}|]
+
+examplePragma :: BSC.ByteString
+examplePragma = [str|{-# OPTIONS_GHC -fdefer-type-errors #-}|]
+
+exampleMultilineLanguagePragma :: BSC.ByteString
+exampleMultilineLanguagePragma = [str|{-# LANGUAGE DeriveFunctor, 
+       DeriveAnyClass, MultiWayIf 
+       #-}|]
+
+exampleMultiplePragma :: BSC.ByteString
+exampleMultiplePragma = [str|{-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}|]
+
+exampleMultiplePragmaAndNewLines :: BSC.ByteString
+exampleMultiplePragmaAndNewLines = [str|{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor, 
+       DeriveAnyClass, MultiWayIf 
+       #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}|]
+
+defaultExtensions :: [HaskellPragma]
 defaultExtensions = [ LanguagePragma "OverloadedStrings"
                     , LanguagePragma "DeriveGeneric"
                     , LanguagePragma "FlexibleContexts"
@@ -26,86 +68,119 @@ defaultExtensions = [ LanguagePragma "OverloadedStrings"
                     , LanguagePragma "MultiWayIf"
                     ]
 
-changeMany :: [Pragma] -> [FilePath] -> IO ()
+changeMany :: [HaskellPragma] -> [FilePath] -> IO ()
 changeMany exts paths = do
     filess <- forM paths $ find always (extension ==? ".hs")
     let files = concat filess
     void $ forM files $ changeOneFile exts
 
-changeOneFile :: [Pragma] -> FilePath -> IO ()
+changeOneFile :: [HaskellPragma] -> FilePath -> IO ()
 changeOneFile = changeExtensions
 
 modifyInPlace' :: FilePath -> (BSC.ByteString -> BSC.ByteString) -> IO ()
 modifyInPlace' = flip modifyInPlace
 
-changeExtensions :: [Pragma] -> FilePath -> IO ()
-changeExtensions exts f = do
-  modifyInPlace' f (changeContent exts)
+changeExtensions :: [HaskellPragma] -> FilePath -> IO ()
+changeExtensions exts f = modifyInPlace' f (changeContent exts)
 
-changeContent :: [Pragma] -> BSC.ByteString -> BSC.ByteString
-changeContent exts contents = case parseIt of
-                                Left _ -> contents
-                                Right r -> r
-  where
-    --Either (ParseError Char Dec) BSC.ByteString
-    parseIt = do
-      let (l,rest) = BSC.breakSubstring "module" contents
-      pragmas <- parse parser "" l
+changeContent :: [HaskellPragma] -> BSC.ByteString -> BSC.ByteString
+changeContent exts contents =
+    case parseIt exts contents of
+        Left _ -> contents
+        Right r -> r
 
-      let (languagePragmas, otherPragmas) = partition (isLanguagePragma) pragmas
-          fixedLanguagePragmas = concatMap fixLanguagePragma languagePragmas
-      return $ pragmasToBSC ((S.toList $ S.fromList fixedLanguagePragmas) <> otherPragmas)
+parseIt :: [HaskellPragma] -> BSC.ByteString -> Either (ParseError Char Text.Megaparsec.Dec) BSC.ByteString
+parseIt exts contents =
+    let (l,rest) = BSC.breakSubstring "module" contents
+    in (\x -> return $ x <> rest) =<< (parseInner exts l)
 
-pragmasToBSC :: [Pragma] -> BSC.ByteString -- snoc
+parseInner :: [HaskellPragma] -> BSC.ByteString -> Either (ParseError Char Text.Megaparsec.Dec) BSC.ByteString
+parseInner exts l = do
+    pragmas <- parse parser "" l
+
+    let (languagePragmas, otherPragmas) = partition (isLanguagePragma) pragmas
+        fixedLanguagePragmas = concatMap fixLanguagePragma languagePragmas
+    return $ pragmasToBSC ((S.toList $ S.fromList (fixedLanguagePragmas <> exts)) <> otherPragmas)
+
+pragmasToBSC :: [HaskellPragma] -> BSC.ByteString -- snoc
 pragmasToBSC pragmas = mconcat $ catMaybes $ map (\p -> (\x -> x <> "\n") <$> unPragma p) pragmas
 
-parser :: Parser [Pragma]
-parser = sepBy apragma newline
+parser :: Parser [HaskellPragma]
+parser = do
+    space
+    allPragma <- allPragmas
+    space
+    eof
+    return allPragma
 
-data Pragma = LanguagePragma BSC.ByteString
-            | LanguagePragmaList [BSC.ByteString]
-            | OtherPragma BSC.ByteString
-            deriving (Eq, Ord)
-
+unPragma :: HaskellPragma -> Maybe BSC.ByteString
 unPragma (LanguagePragma b) = Just b
 unPragma (LanguagePragmaList _) = Nothing
 unPragma (OtherPragma b) = Just b
 
-isLanguagePragma :: Pragma -> Bool
+isLanguagePragma :: HaskellPragma -> Bool
 isLanguagePragma (LanguagePragma _) = True
 isLanguagePragma (LanguagePragmaList _) = True
 isLanguagePragma _ = False
 
-apragma :: Parser Pragma
+allPragmas :: Parser [HaskellPragma]
+allPragmas = do
+    many (apragma <* (optional newline))
+
+apragma :: Parser HaskellPragma
 apragma = do
-  bstring "{-#"
-  space
-  prag <- languagePragmaList <|> languagePragma <|> (liftM (OtherPragma . BSC.pack) $ many letterChar)
-  space
-  bstring "#-}"
-  return prag
+    prag <- (try (languagePragmaList <?> "Language List")) <|> (try (languagePragma <?> "Language")) <|> (otherPragma <?> "Other")
+    return prag
 
-languagePragma :: Parser Pragma
+
+satisfyPragmaStart :: Parser ()
+satisfyPragmaStart = bstring "{-#" *> space
+
+satisfyPragmaEnd :: Parser ()
+satisfyPragmaEnd = space <* bstring "#-}"
+
+otherPragma :: Parser HaskellPragma
+otherPragma = do
+    prag <- between satisfyPragmaStart
+                    satisfyPragmaEnd
+                    (do
+                        t <- many (letterChar <|> (oneOf ['_','-']))
+                        space
+                        v <- many (letterChar <|> digitChar <|> (oneOf ['_','-']))
+                        return $ t <> v)
+    return $ OtherPragma $ BSC.pack prag
+
+languagePragma :: Parser HaskellPragma
 languagePragma = do
-  bstring "LANGUAGE"
-  space
-  prag <- many letterChar
-  return $ LanguagePragma $ BSC.pack prag
+    prag <- between satisfyPragmaStart
+                    satisfyPragmaEnd
+                    (do bstring "LANGUAGE"
+                        space
+                        prag' <- many (letterChar <|> digitChar)
+                        space
+                        return prag'
+                    )
+    return $ LanguagePragma $ BSC.pack prag
 
+bstring :: String -> Parser String
 bstring = tokens (==)
 
-languagePragmaList :: Parser Pragma
+languagePragmaList :: Parser HaskellPragma
 languagePragmaList = do
-  bstring "LANGUAGE"
-  space
-  prag <- flip sepBy (char ',') $ do
-    space
-    inner <- many letterChar
-    space
-    return $ BSC.pack inner
-  return $ LanguagePragmaList prag
+    prag <- between (do satisfyPragmaStart
+                        bstring "LANGUAGE"
+                        space
+                    )
+                    satisfyPragmaEnd
+                    (flip sepBy (char ',') $ do
+                        space
+                        inner <- many (letterChar <|> digitChar)
+                        space
+                        return $ BSC.pack inner
+                    )
+    return $ LanguagePragmaList prag
 
-fixLanguagePragma :: Pragma -> [Pragma]
+fixLanguagePragma :: HaskellPragma -> [HaskellPragma]
 fixLanguagePragma (LanguagePragmaList p) = map LanguagePragma p
 fixLanguagePragma x = [x]
 
